e04f6dda0e32f8fa9b64a593e350c6d3f8d3d371
pavanbca123 <patilsanchita025@gmail.com>
2025-05-06 15:31:23 +0530

    many

---
+++ BasicOperations.java
@@ -0,0 +1,15 @@
+
+public class BasicOperations {
+    public static void main(String[] args) {
+        int a=15;
+        int b=5;
+        int sum=a+b;
+        int diff =a-b;
+        int multi=a*b;
+
+        System.out.println("addition:"+sum);
+        System.out.println("subtraction:"+diff);
+        System.out.println("multipication:"+multi);
+    }
+    
+}
