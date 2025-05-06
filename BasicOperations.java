915c56e3daa4dad3a4508cfd94e96e4f13e8c25b
pavanbca123 <pavansatish201@gmail.com>
2025-05-06 16:25:38 +0530

    Update BasicOperations.java

--- BasicOperations.java
+++ BasicOperations.java
@@ -1,15 +1,13 @@
-
 public class BasicOperations {
     public static void main(String[] args) {
-        int a=15;
-        int b=5;
-        int sum=a+b;
-        int diff =a-b;
-        int multi=a*b;
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        int diff = a - b;
+        int multi = a * b;
 
-        System.out.println("addition:"+sum);
-        System.out.println("subtraction:"+diff);
-        System.out.println("multipication:"+multi);
+        System.out.println("addition: " + sum);
+        System.out.println("subtraction: " + diff);
+        System.out.println("multiplication: " + multi);  
     }
-    
 }
